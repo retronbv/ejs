72fd1c614320f3ed19e0c3db9fb990d882b9dcf8
William (Wgyt) Horning <williamhorning@wgyt.tk>
2021-03-14 12:47:47 -0400

    minified

--- tests/test.js
+++ tests/test.js
@@ -1,2 +1,7 @@
-ejs = require('../lib/ejs.js')
-console.log(ejs.render(`OK, so have fun! :D @<%= user %>`,{user:"hi"}))
+ejs = require('../lib/ejs.js');
+data = ejs.render(`OK, so have fun! :D @<%= user %>`,{user:"hi"})
+if (data==='OK, so have fun! :D @hi'){
+    console.log('sucess!');
+}else{
+    throw new Error(`failure! ${data} isn't equal to OK, so have fun! :D @hi`);
+}
